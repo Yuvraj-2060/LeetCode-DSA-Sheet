8ff1764b5d2fb2bdd8e8f9da408e5f2cb547f911
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2022-09-17 13:36:31 +0530

    Update Leetcode_PNO-75.java

--- Leetcode_PNO-75.java
+++ Leetcode_PNO-75.java
@@ -1,6 +1,33 @@
+#Approach-1 (using predefined sort function)
+
 class Solution {
     public void sortColors(int[] nums) {
         Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+         System.out.println(nums[i]);
+        
+    }
+}
+
+#Approach -2
+   
+ class Solution {
+    public void sortColors(int[] nums) {
+        int temp;
+        //Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {   if(nums[j]<nums[i])
+                {   temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                
+                }
+            
+            }  
+        }
+        
         for(int i=0;i<nums.length;i++)
          System.out.println(nums[i]);
         
