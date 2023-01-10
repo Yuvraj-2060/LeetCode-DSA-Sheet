13bbe60d161206c06e4ba2b7b1d29e0e4bb00adb
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2023-01-10 23:26:38 +0530

    Create Main.java
    Given an array of N integers where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets such that: 
    
    Each student gets one packet.
    The difference between the number of chocolates in the packet with maximum chocolates and the packet with minimum chocolates given to the students is minimum.
    Examples:
    
    Input : arr[] = {7, 3, 2, 4, 9, 12, 56} , m = 3 
    Output: Minimum Difference is 2 
    Explanation:
    We have seven packets of chocolates and we need to pick three packets for 3 students 
    If we pick 2, 3 and 4, we get the minimum difference between maximum and minimum packet sizes.
    
    Input : arr[] = {3, 4, 1, 9, 56, 7, 9, 12} , m = 5 
    Output: Minimum Difference is 6

---
+++ Main.java
@@ -0,0 +1,39 @@
+// JAVA Code For Chocolate Distribution
+// Problem :
+
+import java.util.*;
+
+class Main {
+	static int findMinDiff(int arr[], int n, int m)
+	{
+	    if(n==0 || m==0)
+	        return 0;
+	    if(n<m)
+	        return -1;
+	        
+	        
+	       Arrays.sort(arr);
+	       
+	       int min_diff=Integer.MAX_VALUE;
+	       
+	       for(int i=0;i+m-1<n;i++){
+	           int diff = arr[i+m-1]-arr[i];
+	           if(min_diff>diff)
+	            min_diff=diff;
+	       }
+	       return min_diff;
+	}
+
+	/* Driver program to test above function */
+	public static void main(String[] args)
+	{
+		int arr[] = { 12, 4, 7, 9, 2, 23, 25, 41, 30,
+					40, 28, 42, 30, 44, 48, 43, 50 };
+
+		int m = 7; // Number of students
+
+		int n = arr.length;
+		System.out.println("Minimum difference is "
+						+ findMinDiff(arr, n, m));
+	}
+}
