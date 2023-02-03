1fcb767fde66a1f18308b811d100c5e55cc6a7c5
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2023-02-03 17:53:18 +0530

    Create 50-LeetCode(Pow(x,n)).java
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
    
     
    
    Example 1:
    
    Input: x = 2.00000, n = 10
    Output: 1024.00000
    Example 2:
    
    Input: x = 2.10000, n = 3
    Output: 9.26100
    Example 3:
    
    Input: x = 2.00000, n = -2
    Output: 0.25000
    Explanation: 2-2 = 1/22 = 1/4 = 0.25

---
+++ 50-LeetCode(Pow(x,n)).java
@@ -0,0 +1,19 @@
+// Approach-1
+
+class Solution {
+    public double myPow(double x, int n) {
+         if (n == 0) return 1.0;
+        else if (n % 2 == 1) return x * myPow(x, n - 1);
+        else if (n % 2 == 0) return myPow(x * x, n / 2);
+        else return 1 / myPow(x, -n);
+       
+    }
+}
+
+// Approach-2
+
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+    }
+}
