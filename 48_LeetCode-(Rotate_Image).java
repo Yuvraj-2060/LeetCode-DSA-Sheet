b19289f2f996e784a4fdf8905453e6b6de7cd030
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2023-02-05 00:55:31 +0530

    Create 48_LeetCode-(Rotate_Image).java
    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
    
    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
    
    Example 1:
    
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [[7,4,1],[8,5,2],[9,6,3]]
    
    Example 2:
    
    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
     
    
    Constraints:
    
    n == matrix.length == matrix[i].length
    1 <= n <= 20
    -1000 <= matrix[i][j] <= 1000

---
+++ 48_LeetCode-(Rotate_Image).java
@@ -0,0 +1,52 @@
+// Approach : 1
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n = matrix.length;
+        int rotated[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = rotated[i][j];
+            }
+        }
+        
+        
+    }
+}
+
+
+//Approach 2: Time Complexity: O(N*N) + O(N*N).One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
+
+            // Space Complexity: O(1).
+
+//Step 1: Transpose the matrix. (transposing means changing columns to rows and rows to columns)
+//Step 2: Reverse each row of the matrix.
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+        
+    }
+}
