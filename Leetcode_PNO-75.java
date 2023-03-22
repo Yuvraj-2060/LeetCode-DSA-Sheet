3d81d09ecde5f0cf7ac5bbb26a681435c2d64efe
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2023-03-22 22:03:34 +0530

    Update Leetcode_PNO-75.java

--- Leetcode_PNO-75.java
+++ Leetcode_PNO-75.java
@@ -1,4 +1,4 @@
-#Approach-1 (using predefined sort function)
+// #Approach-1 (using predefined sort function)
 
 class Solution {
     public void sortColors(int[] nums) {
@@ -9,7 +9,7 @@ class Solution {
     }
 }
 
-#Approach -2
+// #Approach -2 (Using any sorting algorithm)   [  Time Complexity : O(N2)  ||  Space Complexity : O(N)    ]
    
  class Solution {
     public void sortColors(int[] nums) {
@@ -32,4 +32,103 @@ class Solution {
          System.out.println(nums[i]);
         
     }
+}
+
+//Approach-3 *(Dutch National Flag)*     [  Time Complexity : O(N)  ||  Space Complexity : O(1)    ]
+
+import java.util.*;
+
+public class Main {
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (arr.get(mid) == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid, temp);
+
+                low++;
+                mid++;
+
+            } else if (arr.get(mid) == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr.get(mid);
+                arr.set(mid, arr.get(high));
+                arr.set(high, temp);
+
+                high--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 6;
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(new Integer[] {0, 2, 1, 2, 0, 1}));
+        sortArray(arr, n);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+
+}
+Output:
+After sorting:
+0 0 1 1 2 2  
+
+Time Complexity: O(N), where N = size of the given array.
+Reason: We are using a single loop that can run at most N times.
+
+Space Complexity: O(1) as we are not using any extra space.
+    
+ ****************************************************   Same as above but by using Normal ARRAY    ******************************************************
+ 
+import java.util.*;
+
+public class Main {
+    public static void sortArray(int arr[], int n) {
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+
+                low++;
+                mid++;
+
+            } else if (arr[mid] == 1) {
+                mid++;
+
+            } else {
+                
+                int temp = arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+
+                high--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 6;
+        int arr[]={0, 2, 1, 2, 0, 1};
+        sortArray(arr, n);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+    }
+
 }
