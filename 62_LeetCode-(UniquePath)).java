6c5ece512323da6748482d2c94ae8a82a954d695
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2023-04-02 13:44:21 +0530

    Update 62_LeetCode-(UniquePath)).java
    62. Unique Paths
    Medium
    
    13403
    
    377
    
    Add to List
    
    Share
    There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
    
    Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
    
    The test cases are generated so that the answer will be less than or equal to 2 * 109.
    
     
    
    Example 1:
    
    
    Input: m = 3, n = 7
    Output: 28
    Example 2:
    
    Input: m = 3, n = 2
    Output: 3
    Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
    1. Right -> Down -> Down
    2. Down -> Down -> Right
    3. Down -> Right -> Down
     
    
    Constraints:
    
    1 <= m, n <= 100

--- 62_LeetCode-(UniquePath)).java
+++ 62_LeetCode-(UniquePath)).java
@@ -22,7 +22,23 @@ class Solution {
         
     }
 }
+******************************************************(1)************************************************************
+// Time Complexity: The time comp[lexity of this recursive solution is exponential.
 
+// Space Complexity: As we are using stack space for recursion so the space complexity will also be exponential.
+
+class Solution {
+    public int countPaths(int i,int j,int n,int m)
+    {
+        if(i==(n-1)&&j==(m-1)) return 1;
+        if(i>=n||j>=m) return 0;
+        else return countPaths(i+1,j,n,m)+countPaths(i,j+1,n,m);
+    }
+    public int uniquePaths(int m, int n) {
+       return countPaths(0,0,m,n);
+    }
+}
+********************************************************************************************************************
 // Approach-2
 
 class Solution {
@@ -38,5 +54,43 @@ class Solution {
             }
         }
         return dp[0][0];   
+    }
+}
+
+**************************************************************(2)****************************************************************************
+// Approach-3 same DP Solution [ Time Complexity: O(n*m) because at max there can be n*m number of states. || Space Complexity : O(n*m)    ]
+class Solution {
+     public int countPaths(int i, int j, int n, int m, int[][] dp) {
+        if (i == (n - 1) && j == (m - 1)) return 1;
+        if (i >= n || j >= m) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j] = countPaths(i + 1, j, n, m, dp) + countPaths(i, j + 1, n, m, dp);
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        int num = countPaths(0, 0, m, n, dp);
+        if (m == 1 && n == 1) {
+            return num;
+        }
+        return dp[0][0];
+    }
+}
+
+**********************************************************************(3)*******************************************************************************************
+// Approach-4  [ Time Complexity: The time complexity of this solution will be O(n-1) or  O(m-1) depending on the formula we are using || Space Complexity : O(n)   ]
+
+class Solution {
+     public int uniquePaths(int m, int n) {
+        int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
     }
 }
