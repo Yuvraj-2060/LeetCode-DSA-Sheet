f499f4e1146dfc13b32fcabeecaaff2c013fa87b
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2023-04-12 14:36:11 +0530

    Update No_Subarray_With_XorK.java
    Problem Description
     
     
    
    Given an array of integers A and an integer B.
    
    Find the total number of subarrays having bitwise XOR of all elements equals to B.
    
    
    
    Problem Constraints
    1 <= length of the array <= 105
    
    1 <= A[i], B <= 109
    
    
    
    Input Format
    The first argument given is the integer array A.
    
    The second argument given is integer B.
    
    
    
    Output Format
    Return the total number of subarrays having bitwise XOR equals to B.
    
    
    
    Example Input
    Input 1:
    
     A = [4, 2, 2, 6, 4]
     B = 6
    Input 2:
    
     A = [5, 6, 7, 8, 9]
     B = 5
    
    
    Example Output
    Output 1:
    
     4
    Output 2:
    
     2
    
    
    Example Explanation
    Explanation 1:
    
     The subarrays having XOR of their elements as 6 are:
     [4, 2], [4, 2, 2, 6, 4], [2, 2, 6], [6]
    Explanation 2:
    
     The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9]

--- No_Subarray_With_XorK.java
+++ No_Subarray_With_XorK.java
@@ -36,3 +36,33 @@ public int solve(int[] A, int B) {
         }
         return c; 
     }
+
+-------------------------------------------------------(Better Solution Using HashMap)-------------------------------------------------------------------------------------
+	
+ublic class Solution {
+    public int solve(int[] A, int B) {
+       int c = 0;
+        int currentXOR = 0;
+        HashMap<Integer, Integer> xorCount = new HashMap<>();
+        xorCount.put(0, 1); // Initialize with 0 XOR count
+        
+        for (int i = 0; i < A.length; i++) {
+            currentXOR ^= A[i]; // Update current XOR
+            
+            int targetXOR = currentXOR ^ B; // Calculate the target XOR
+            
+            if (xorCount.containsKey(targetXOR)) {
+                c += xorCount.get(targetXOR); // Increment count by the target XOR count
+            }
+            
+            if (xorCount.containsKey(currentXOR)) {
+                xorCount.put(currentXOR, xorCount.get(currentXOR) + 1); // Update current XOR count
+            } else {
+                xorCount.put(currentXOR, 1);
+            }
+        }
+        
+        return c;
+    }
+}
+
